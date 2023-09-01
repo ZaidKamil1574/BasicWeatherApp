df6be23c41837dea28087977fdb97d1afdbe3634
Zaid Kamil <64871419+ZaidKamil1574@users.noreply.github.com>
2023-09-01 10:02:58 -0700

    Add files via upload

---
+++ WeatherUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUtilApp.swift
+//  WeatherUtil
+//
+//  Created by Zaid Kamil on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
